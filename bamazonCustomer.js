259a44653477ee1abaccc08ae6099faa31194979
bee25141 <bee25141@gmail.com>
2019-06-01 22:17:57 -0500

    npm installed enquirer and able to view all items in store, working on looping through array of store objects to display properly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const enquirer = require("enquirer");
 const userName = "";
 const connection = mysql.createConnection({
     host: "localhost",
@@ -16,3 +17,17 @@ connection.connect(function (err) {
 
 let queryType = process.argv[2];
 let userInput = process.argv.slice(3).join(" ");
+
+function storeView() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(Object.keys(RowDataProduct));
+        // for (i=0; i<res.RowDataPacket.length; i++){
+        //     console.log(res[i])
+        // }
+        connection.end();
+    });
+};
+storeView();
